0940b50e84aeee524d84b5546e6d17dbc5e44370
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-02-01 09:13:38 +0100

    Add basic page structure with routing and Tailwind CSS integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,32 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import GlobalContext from "./contexts/GlobalContext";
+
+/*PAGES*/
+import Homepage from "./pages/Homepage/Homepage";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound";
+
+/*LAYOUTS*/
+import Default from "./layouts/Default";
+import Blank from "./layouts/Blank";
+
+
 function App() {
 
   return (
-    <>
-    </>
+    <GlobalContext.Provider value={{}}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Default />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+          <Route element={<Blank />}>
+            <Route path="*" element={<NotFound />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
---
+++ client/src/layouts/Header/Header.jsx
@@ -0,0 +1,9 @@
+import { NavLink } from "react-router-dom"
+
+export default function Header(){
+    return (
+        <header>
+            <p>Header</p>
+        </header>
+    )
+}
